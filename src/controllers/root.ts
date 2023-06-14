e8b2f0ee6bf142bce75d2f670ff3371c2547ad96
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2023-06-14 18:41:09 +0100

    feat: update version

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -12,7 +12,7 @@ export function root(ctx: any) {
         title: 'Homepage & API Capabilities',
         about: {
             source: 'https://github.com/abiddiscombe/countries',
-            version: '4.1.0',
+            version: '4.2.0',
             cors: {
                 origin: ctx.response.headers.get('Access-Control-Allow-Origin'),
             },
